321d1fd08493f26fdcc33a156b41027868a29a48
BruDevin <bruno.ads.2022@gmail.com>
2023-07-28 14:53:19 -0300

    fim do curso - alura-java-listas-colecoes

--- src/Principal/PrincipalComListas.java
+++ src/Principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -16,7 +16,7 @@ public class PrincipalComListas {
         filme3.avalia(10);
         Serie lost = new Serie("Lost", 2000);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filme3);
         lista.add(meuFilme);
         lista.add(outroFilme);
@@ -27,5 +27,20 @@ public class PrincipalComListas {
                 System.out.println("Classificacao " + ((Filme) item).getClassificacao());
             }
         }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Mila Kunis");
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Jason Statham");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println(lista);
     }
 }
