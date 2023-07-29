9e5a09c6e5d0788d5947fbfd86d25c77f7d000d5
BruDevin <bruno.ads.2022@gmail.com>
2023-07-29 11:31:31 -0300

    Fim da segunda aula. Alura. Formação Java com orientação a objetos. Curso Java, consumindo API, gravando arquivos e lidando com erros.

--- src/Principal/PrincipalComBusca.java
+++ src/Principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package Principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOMDB;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -22,7 +28,19 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
         System.out.println(response.body());
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        //Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
+        System.out.println(meuTituloOMDB);
+        Titulo meuTitulo = new Titulo(meuTituloOMDB);
+        System.out.println("Título convertido: ");
+        System.out.println(meuTitulo);
 
 
     }
